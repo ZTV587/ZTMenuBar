968baf4b2dfc660c71ef8e6cdc7fa72f0471a276
zzt <zzt@qq.com>
2016-09-03 08:33:18 +0800

    Switch Controllers

--- ZTMenuBbar/TitleViewControllers/ThirdViewController.m
+++ ZTMenuBbar/TitleViewControllers/ThirdViewController.m
@@ -11,6 +11,7 @@
 @implementation ThirdViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor yellowColor];
